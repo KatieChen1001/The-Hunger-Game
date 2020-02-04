363bde6b98528977cc68ec6dd71ffd335d73ac6a
K80CHEN <mzc219@nyu.edu>
2020-02-04 18:21:49 +0800

    added comments to Backend

--- Capstone Revisited/backend/server.js
+++ Capstone Revisited/backend/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // require returns the express function
+const app = express(); //call the express function and store it in an object
 const connectDB = require("./DB/db.js");
 const userController = require("./Controller/user.js");
 const cors = require("cors");
@@ -9,10 +9,19 @@ const corsOption = {
   optionSuccessStatus: 200
 };
 
+const port = process.env.PORT || 3000;
+// In real production environment, ports are dynamically assigned
+// It's good practice to use the PORT environment variable
+
 connectDB();
+
 app.use(cors(corsOption));
+
 app.use(express.json({ extended: false }));
+//??? still dont know why "extende: false"
+
 app.use("/", userController);
-app.listen(3000, () => {
-  console.log("Server started");
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
